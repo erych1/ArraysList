dde2dad63431d135c1ab9e9ee98b1c112921a9fb
Kudbuhon08 <qweimxs@gmail.com>
2024-02-09 16:11:47 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> list = new ArrayList<>();
+        list.add(171);
+        list.add(161);
+        list.add(151);
+        list.add(141);
+        list.add(131);
+        list.add(121);
+        for (int num: list) {
+            System.out.println(num);
+
+        }
+        System.out.println("----------");
+        Collections.sort(list);
+        for (int num: list) {
+            System.out.println(num);
+
+        }
     }
 }
